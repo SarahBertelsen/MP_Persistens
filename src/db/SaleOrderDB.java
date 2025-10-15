9f1b1d895aa6c0fe8935aec594828694e6574a61
SarahBertelsen <10669007@ucn.dk>
2025-10-15 10:07:26 +0200

    Lidt SaleOrder

--- src/db/SaleOrderDB.java
+++ src/db/SaleOrderDB.java
@@ -2,17 +2,28 @@ package db;
 
 import java.sql.PreparedStatement;
 
+import model.Product;
+import model.Discount;
+import model.Freight;
 import model.SaleOrder;
 
-public class SaleOrderDB {
-	private static final String FIND_BY_ID_Q = "";
-	private PreparedStatement findByIdPS;
+public class SaleOrderDB implements SaleOrderDAO {
+	private static final String INSERT_SALEORDER_Q = "";
+	private PreparedStatement insertSaleOrderPS;
+	
+	private CustomerDAO cDao;
+	private ProductDAO pDao;
+	private DiscountDAO dDao;
+	private FreightDAO fDao;
 	
 	public SaleOrderDB() {
 		
 	}
-	
-	private SaleOrder buildObject(SaleOrder saleOrder) {
-		return null;
+
+	@Override
+	public void saveSaleOrder(SaleOrder saleOrder) {
+		// TODO Auto-generated method stub
+		
 	}
+	
 }
